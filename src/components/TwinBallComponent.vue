9b8ad5b07127a4e84757e12746c474e685170191
unknown <erick.ishimine@gmail.com>
2024-01-08 00:27:47 -0300

    enhancement: change ball color according to effect

--- src/components/TwinBallComponent.vue
+++ src/components/TwinBallComponent.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useBoardStore } from '@/stores/useBoardStore'
+import { Effect } from '@/types/TwinBall';
 import { computed } from 'vue'
 
 type TwinBallComponentProps = {
@@ -9,8 +10,18 @@ type TwinBallComponentProps = {
 const props = defineProps<TwinBallComponentProps>()
 const boardStore = useBoardStore()
 const state = computed(() => props.index === 1 ? boardStore.twinBallOne : boardStore.twinBallTwo)
+const color = computed(() => {
+  switch (state.value.effect) {
+    case Effect.FAST:
+      return 'bg-gradient-to-r from-red-800 to-red-400 animate-spin'
+    case Effect.REVERSE:
+      return 'bg-gradient-to-r from-teal-800 to-teal-400 animate-spin'
+    default:
+      return 'bg-white'
+  }
+})
 </script>
 
 <template>
-  <div class="absolute h-8 w-8 rounded-2xl z-20 bg-white transition-all duration-300" :style="{ left: `${state.position.x}rem`, top: `${state.position.y}rem` }" />
+  <div class="absolute h-8 w-8 rounded-2xl z-20 transition-all duration-300" :class="color" :style="{ left: `${state.position.x}rem`, top: `${state.position.y}rem` }" />
 </template>
